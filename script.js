6e22f5f942b8d287bdde33e3c4038b2434982f0a
Arthur Elleres <arthur.elleres@itec.ufpa.br>
2023-02-04 11:34:43 -0300

    Adding winner logic

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ function getComputerChoice() {
     let list = ["Rock", "Paper", "Scissors"]
     return list[getRandomInt(3)]
 }
+function playRound(player, computer) {
+    if (player == computer) {
+        return "Draw"
+    } else if (player == "Rock" && computer == "Scissors" || player == "Paper" && computer == "Rock" || player == "Scissors" && computer == "Paper") {
+        return "You won!"
+    } else {
+        return "You lost!"
+    }
+    
+}
